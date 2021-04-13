c2bbfc5740da46525402135af1e24db6325b6bde
JoshuaMac-USC <macaldojoshua23@gmail.com>
2021-04-13 20:57:40 +0800

    Primary Commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="hero">
+    
+    <div class="hero__text container">
+
+      <h1>UNSTOPPABLE</h1>
+      <h2>Car Rentals</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia magni atque nostrum cumque excepturi sint reiciendis magnam! Perspiciatis minus corporis quidem! Modi culpa saepe repudiandae deleniti cum perferendis excepturi. Enim.</p>
+
+      <a href="#" class="button hero__cta">
+        <span>Rent Now</span>
+     </a>
+    </div>
+
+    <div class="hero__image">
+        <img src="../assets/hero.svg" alt="">
+    </div>    
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+}
+</script>
+
+<style lang="scss">
+
+</style>
